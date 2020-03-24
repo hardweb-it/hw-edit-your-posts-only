afdb005b21ccab56c61f9265150e8a1152ca446f
Gianluca Marzaro <3406023+hardweb-it@users.noreply.github.com>
2020-03-24 23:44:24 +0100

    Update hw-edit-your-posts-only.php

--- hw-edit-your-posts-only.php
+++ hw-edit-your-posts-only.php
@@ -8,6 +8,7 @@ Version: 1.0
 Description: Makes it so normal users can see only their posts and drafts from the manage posts screen.  Great for multi-user blogs where you want users to only see posts that they have created.  Wordpress already makes it so they cannot edit the posts, but still allows them to see the titles.  This can get annoying to find your posts mixed in with thousands of others.
 */
 
+add_filter('parse_query', 'hw_edit_your_posts_only' );
 function hw_edit_your_posts_only( $wp_query ) {
     if ( strpos( $_SERVER[ 'REQUEST_URI' ], '/wp-admin/edit.php' ) !== false ) {
         if ( !current_user_can( 'level_10' ) ) {
@@ -16,6 +17,4 @@ function hw_edit_your_posts_only( $wp_query ) {
         }
     }
 }
-
-add_filter('parse_query', 'hw_edit_your_posts_only' );
 ?>
